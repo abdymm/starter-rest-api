d69207917c47a9fbc6fab870d4e162467696010c
“abdymm” <abdymalikmulky@gmail.com>
2023-06-22 04:42:24 +0700

    feat: add travels

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express = require('express')
+const axios = require("axios")
+const express = require("express")
 const app = express()
-const db = require('@cyclic.sh/dynamodb')
+const db = require("@cyclic.sh/dynamodb")
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -20,49 +21,82 @@ app.use(express.urlencoded({ extended: true }))
 // #############################################################################
 
 // Create or Update an item
-app.post('/:col/:key', async (req, res) => {
+app.post("/:col/:key", async (req, res) => {
   console.log(req.body)
 
   const col = req.params.col
   const key = req.params.key
-  console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
+  console.log(
+    `from collection: ${col} delete key: ${key} with params ${JSON.stringify(
+      req.params
+    )}`
+  )
   const item = await db.collection(col).set(key, req.body)
   console.log(JSON.stringify(item, null, 2))
   res.json(item).end()
 })
 
 // Delete an item
-app.delete('/:col/:key', async (req, res) => {
+app.delete("/:col/:key", async (req, res) => {
   const col = req.params.col
   const key = req.params.key
-  console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
+  console.log(
+    `from collection: ${col} delete key: ${key} with params ${JSON.stringify(
+      req.params
+    )}`
+  )
   const item = await db.collection(col).delete(key)
   console.log(JSON.stringify(item, null, 2))
   res.json(item).end()
 })
 
 // Get a single item
-app.get('/:col/:key', async (req, res) => {
+app.get("/:col/:key", async (req, res) => {
   const col = req.params.col
   const key = req.params.key
-  console.log(`from collection: ${col} get key: ${key} with params ${JSON.stringify(req.params)}`)
+  console.log(
+    `from collection: ${col} get key: ${key} with params ${JSON.stringify(
+      req.params
+    )}`
+  )
   const item = await db.collection(col).get(key)
   console.log(JSON.stringify(item, null, 2))
   res.json(item).end()
 })
 
+// Get a travels
+app.get("/travels", async (req, res) => {
+  // Make a request for a user with a given ID
+  axios
+    .get("https://simpu.kemenag.go.id/apps/api/travel")
+    .then(function (response) {
+      // handle success
+      res.json(response).end()
+    })
+    .catch(function (error) {
+      // handle error
+      res.json(error).end()
+    })
+    .finally(function () {
+      // always executed
+    })
+
+})
+
 // Get a full listing
-app.get('/:col', async (req, res) => {
+app.get("/:col", async (req, res) => {
   const col = req.params.col
-  console.log(`list collection: ${col} with params: ${JSON.stringify(req.params)}`)
+  console.log(
+    `list collection: ${col} with params: ${JSON.stringify(req.params)}`
+  )
   const items = await db.collection(col).list()
   console.log(JSON.stringify(items, null, 2))
   res.json(items).end()
 })
 
 // Catch all handler for all other request.
-app.use('*', (req, res) => {
-  res.json({ msg: 'no route handler found' }).end()
+app.use("*", (req, res) => {
+  res.json({ msg: "no route handler found" }).end()
 })
 
 // Start the server
