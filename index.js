1e8fa866a03a20bf155e1d04faaa001dbc1ee500
“abdymm” <abdymalikmulky@gmail.com>
2023-06-22 07:38:28 +0700

    fix list

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ app.get("/:col", async (req, res) => {
   console.log(
     `list collection: ${col} with params: ${JSON.stringify(req.params)}`
   )
-  const items = await db.collection(col)
+  const items = await db.collection(col).list()
   console.log(JSON.stringify(items, null, 2))
   res.json(items).end()
 })
